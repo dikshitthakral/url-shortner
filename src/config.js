81b1e1c8098a45be6d7657eedcb01972489d5299
Dikshit Thakral <dikshit.thakral@dkatalis.com>
2021-02-28 21:24:25 +0700

    Updated mongodb url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 const config = {
-    //"mongoURI": "mongodb://localhost:27017/local_db",
-    "mongoURI": "mongodb+srv://admin:admin@cluster0.9iloj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "mongoURI": "mongodb://localhost:27017/local_db",
     "baseURL": "http://url-shortner/v1",
     "port": 5000
 }
